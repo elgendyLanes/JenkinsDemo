20924e12ece517652d63db14760b1ddf5e57b1ab
melgendy <mohamed.elgendy@blacklane.com>
2018-09-26 17:36:10 +0200

    add more classes

---
+++ app/src/main/java/com/blackalne/melgendy/jenkinsdemo/view/JustView.kt
@@ -0,0 +1,11 @@
+package com.blackalne.melgendy.jenkinsdemo.view
+
+class JustView {
+  fun add(num1: Int, num2: Int): Int {
+    return num1 + num2
+  }
+
+  fun subtract(num1: Int, num2: Int): Int {
+    return num1.minus(num2)
+  }
+}
